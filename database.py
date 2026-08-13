1c22fe84adefbb4394d6ad569c7bfad7e44895d1
MilanessaGit <miguel.aguirre.v12@gmail.com>
2026-08-13 12:25:42 -0400

    Preparar conexion BD para produccion

--- database.py
+++ database.py
@@ -1,15 +1,16 @@
+import os
 import mysql.connector
 import pandas as pd
 
 
 def get_connection():
     return mysql.connector.connect(
-        host="127.0.0.1",
-        user="root",
-        password="",
-        database="back_api_laravel"
+        host=os.getenv("DB_HOST", "127.0.0.1"),
+        port=int(os.getenv("DB_PORT", "3306")),
+        user=os.getenv("DB_USERNAME", "root"),
+        password=os.getenv("DB_PASSWORD", ""),
+        database=os.getenv("DB_DATABASE", "back_api_laravel")
     )
-
 
 def get_product_data():
     """
